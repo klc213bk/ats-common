5f22814b009b7256d27fa26e0c133578841926b8
steven <steven@mylinux>
2022-01-26 17:00:09 +0800

    init

--- src/main/java/com/klc213/ats/common/AtsBar.java
+++ src/main/java/com/klc213/ats/common/AtsBar.java
@@ -11,12 +11,13 @@ public class AtsBar {
 	private BigDecimal close;
 	private Long volume;
 	private Integer count;
+	private Long time;
 	
 	public AtsBar() { }
 	
 	public AtsBar(String symbol, BarSizeEnum barSize, BigDecimal open,
 			BigDecimal high, BigDecimal low, BigDecimal close,
-			Long volume, Integer count) { 
+			Long volume, Integer count, Long time) { 
 		this.symbol = symbol;
 		this.barSize = barSize;
 		this.open = open;
@@ -25,6 +26,7 @@ public class AtsBar {
 		this.close = close;
 		this.volume = volume;
 		this.count = count;
+		this.time = time;
 	}
 	
 	public String getSymbol() {
@@ -50,6 +52,46 @@ public class AtsBar {
 	}
 	public Integer getCount() {
 		return count;
+	}
+
+	public Long getTime() {
+		return time;
+	}
+
+	public void setTime(Long time) {
+		this.time = time;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public void setBarSize(BarSizeEnum barSize) {
+		this.barSize = barSize;
+	}
+
+	public void setOpen(BigDecimal open) {
+		this.open = open;
+	}
+
+	public void setHigh(BigDecimal high) {
+		this.high = high;
+	}
+
+	public void setLow(BigDecimal low) {
+		this.low = low;
+	}
+
+	public void setClose(BigDecimal close) {
+		this.close = close;
+	}
+
+	public void setVolume(Long volume) {
+		this.volume = volume;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
 	}
 	
 	
--- src/main/java/com/klc213/ats/common/util/KafkaUtils.java
+++ src/main/java/com/klc213/ats/common/util/KafkaUtils.java
@@ -1,11 +1,18 @@
 package com.klc213.ats.common.util;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class KafkaUtils {
 
@@ -17,5 +24,22 @@ public class KafkaUtils {
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
 		return new KafkaProducer<>(props);
+	}
+	
+	public static String getTwsMktDataTopic(String symbol) {
+		return "TWS.MKTDATA." + symbol;
+	}
+	public static Set<String> listTopics(String kafkaRestUrl) throws Exception {
+		String url = kafkaRestUrl + "/listTopics";
+		String response = HttpUtils.restService(url, "GET");
+
+//		LOG.info(">>>>>>>>>>>> response={} ", response);
+
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode jsonNode = mapper.readTree(response);
+		String topicStr = jsonNode.get("topics").asText();
+		List<String> topicList = mapper.readValue(topicStr, new TypeReference<List<String>>() {});
+		
+		return new HashSet<>(topicList);
 	}
 }
